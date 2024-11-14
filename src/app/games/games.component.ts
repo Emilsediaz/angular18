8c58d969799bd2ab5968b931c5e8f2835486f791
emilsed <emily83_d@hotmail.com>
2024-11-14 11:58:41 -0300

    Comunicacion de hijo a padre

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 @Component({
@@ -9,7 +9,7 @@ import {CommonModule} from '@angular/common';
   <h3>Los juegos favoritos de {{username}}</h3>
    <ul>
     @for (game of games; track game.id) {
-      <li>{{ game.name }}</li>
+      <li (click)="fav(game.name)">{{ game.name }}</li>
     }
    </ul>
   `,
@@ -17,6 +17,13 @@ import {CommonModule} from '@angular/common';
 })
 export class GamesComponent {
   @Input() username='';
+
+  @Output() addFavoriteEvent = new EventEmitter<string>();
+
+  fav (gameName : string) {
+  this.addFavoriteEvent.emit(gameName);
+}
+
  games = [
   {
     id: 1,
